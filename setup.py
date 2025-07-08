c8638b5395895b91dd142e0af593993fba37376b
Kamal Hussain <kamalhussain193213@outlook.com>
2025-07-08 15:23:51 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tvdatafeed-pro',
+    version='0.1.0',
+    description='TradingView data fetcher (modified version)',
+    author='TothyoIT',
+    author_email='your-email@example.com',
+    url='https://github.com/TothyoIT/tvdatafeed-pro',
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+        'pandas',
+        'chromedriver-autoinstaller',
+        'websocket-client'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
 
